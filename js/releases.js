4ecb3343d623dacdd5c1fa9eb6a6040f87c61c02
jwildfire <jwildfire@gmail.com>
2019-03-29 22:48:38 -0700

    tweak formatting for releases. misc layout/wording tweaks.

--- js/releases.js
+++ js/releases.js
@@ -15,6 +15,7 @@ function buildReleaseList(repos, element) {
             return Promise.all(responses.map(response => response.json()));
         })
         .then(json => {
+          console.log(json)
             //Capture latest releases.
             const latestReleases = json
                 .filter(releases => releases.length > 0)
@@ -29,42 +30,34 @@ function buildReleaseList(repos, element) {
                     return releases.sort((a,b) => b.date.getTime() - a.date.getTime())[0]; // get latest release
                 })
                 .sort((a,b) => b.date.getTime() - a.date.getTime())
-                .slice(0,5); // get latest five releases
-
+            var numShown=5;
             selection
                 .selectAll('div.release')
                     .data(latestReleases)
                     .enter()
                 .append('div')
+                .classed("hidden",function(d,i){return i>=numShown})
                 .classed('release', true)
                 .each(function(d) {
                     const release = d3.select(this);
                     release
-                        .append('h3')
-                        .classed('release__repo', true)
-                        .attr('title', 'View repository')
-                            .append('a')
-                            .attr({
-                                'href': d.repo_url,
-                                'target': '_blank'
-                            })
-                            .text(d.repo)
-                                .append('span')
-                                .attr('title', 'View release')
-                                .classed('release__date', true)
-                                    .append('a')
-                                    .attr({
-                                        'href': d.html_url,
-                                        'target': '_blank'
-                                    })
-                                    .text(`Released ${d.created_at.substring(0,10)}`);
+                        .append('h5')
+                        .html(`<a href='${d.html_url}'><i class="fa fa-tag" aria-hidden="true"></i> ${d.name}</a> of <a href='${d.repo_url}'>${d.repo}</a> released on ${d.created_at.substring(0,10)}`)
                     release
-                        .append('p')
+                        .append('span')
                         .classed('release__body', true)
                         .html(d.html);
                 });
+
                 selection
                     .append('p')
-                    .html('<a target = "_blank" href = "https://github.com/RhoInc/">View all repositories</a>');
+                    .append('a')
+                    .html('Show 5 more releases')
+                    .style("text-decoration","underline")
+                    .style("cursor","pointer")
+                    .on("click",function(){
+                      numShown=numShown+5;
+                      selection.selectAll('div.release').classed("hidden",function(d,i){return i>=numShown})
+                    })
         });
 }
