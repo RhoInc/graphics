dcf8f022e77f9becb418345d7c65f06d95fddb34
samussiah <samussiah@gmail.com>
2019-05-21 13:19:38 -0400

    git add -A

---
+++ scripts/getBranches.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const repos = require('../data/repos.json');
+const defineHeaders = require('./defineHeaders');
+
+if (require.main === module)
+    defineHeaders(headers => {
+        module.exports(headers);
+    });
+
+module.exports = function(headers, exit = true) {
+    const branches = Promise.all(
+        repos.map(repo => fetch(repo.branches_url.replace('{/branch}', ''), { headers }))
+    ); // fetch each repo's branches
+
+    return branches
+        .then(responses => Promise.all(responses.map(response => response.json())))
+        .then(json => {
+            //Save branches.
+            fs.writeFileSync('./data/branches.json', JSON.stringify(json, null, 4), 'utf8');
+            console.log('All branches successfully saved to ./data/branches.json!');
+            if (exit) process.exit();
+        })
+        .catch(error => {
+            console.log(`Error fetching branches: ${error}`);
+        });
+};
